fa68cacb23ce816a20c51e58c28ead7f79e88b71
ketan-sutar <sutarketan35@gmail.com>
2024-06-29 15:35:53 +0530

    done almost

--- scritp.js
+++ scritp.js
@@ -1,5 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('body'),
-//     smooth: true
-// });
+const mobile_btn = document.querySelector("#mobile-navbar");
+const header = document.querySelector(".header");
 
+const toggleNavbar = () => {
+  header.classList.toggle("active");
+};
+mobile_btn.addEventListener("click", () => toggleNavbar());
